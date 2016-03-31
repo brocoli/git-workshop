0ed7d516b9e7af2943a6e31b7c9df7a4eaba1bc2
Julio Felipe Angelini <julio.angelini@gmail.com>
2016-03-31 08:10:18 -0300

    New version w/ slides.

---
+++ example_tree.lua
@@ -0,0 +1,15 @@
+
+repository = {
+    commit_1 = {
+        README.md = "This is an example git repository!",
+        some_folder = {
+            foo.txt = "This is the content of the file 'foo'",
+            bar.txt = {
+                some_file = "asdfasdfasdf",
+            },
+        },
+        some_other_folder = {
+            example.txt = "182739182730197263812736918723",
+        },
+    },
+}
